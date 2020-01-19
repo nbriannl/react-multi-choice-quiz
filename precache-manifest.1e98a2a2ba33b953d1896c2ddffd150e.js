08e4acc49a6ac374a6dac0a06825f7cc6995dcd7
Labayna Neil Brian Narido <neilbrian.nl@gmail.com>
2020-01-19 11:55:47 +0800

    Updates

---
+++ precache-manifest.1e98a2a2ba33b953d1896c2ddffd150e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd0e188f5b0cb3642cb7",
+    "url": "/react-multi-choice-quiz/static/css/main.21b66964.chunk.css"
+  },
+  {
+    "revision": "dd0e188f5b0cb3642cb7",
+    "url": "/react-multi-choice-quiz/static/js/main.dd0e188f.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/react-multi-choice-quiz/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "a52a62a6bcddaea718ed",
+    "url": "/react-multi-choice-quiz/static/js/runtime~main.a52a62a6.js"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/react-multi-choice-quiz/static/media/logo.05a3e30f.svg"
+  },
+  {
+    "revision": "b4084e44bea589ec3800fdfdd69cf189",
+    "url": "/react-multi-choice-quiz/static/media/icon-check.b4084e44.svg"
+  },
+  {
+    "revision": "e0f227f1cb3fa64ea25930f2905342b7",
+    "url": "/react-multi-choice-quiz/index.html"
+  }
+];
